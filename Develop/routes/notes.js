3bd47b271dad47e6b6fc2f74771632d55c683868
Nicholas Coiner <npcoiner@ucdavis.edu>
2023-09-08 22:03:25 -0700

    Spacing and syntax

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -20,6 +20,7 @@ const readAndAppend = (content, file) => {
         }
     });
 };
+
 notes.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
