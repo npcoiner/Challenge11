ee5dfc8d7f34ea0ec059c31b5e3a86b0ceff6fde
Nicholas Coiner <npcoiner@ucdavis.edu>
2023-09-08 21:56:07 -0700

    Add readFromFile from util

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,4 +1,7 @@
 const notes = require('express').Router();
+const util = require('util');
+
+const readFromFile = util.promisify(fs.readFile);
 
 notes.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
