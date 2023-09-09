be6bbe1693055b3126799f6fc846e65b394b4586
Nicholas Coiner <npcoiner@ucdavis.edu>
2023-09-08 22:01:34 -0700

    Added post to add data in notes route

--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -5,10 +5,31 @@ const fs = require('fs');
 
 
 const readFromFile = util.promisify(fs.readFile);
+const writeToFile = (destination, content) =>
+    fs.writeFile(destination, JSON.stringify(content, null, 4), (err) =>
+        err ? console.error(err) : console.info(`\nData written to ${destination}`)
+);
 
 notes.get('/', (req, res) => {
     readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
+notes.post('/', (req, res) => {
+    console.log(req.body);
   
+    const { title, text} = req.body;
+  
+    if (req.body) {
+        const newPost = {
+            title,
+            text
+        };
+  
+        readAndAppend(newPost, './db/db.json');
+        res.json(`Data added successfully 🚀`);
+    } else {
+        res.error('Error in adding tip');
+    }
+});
+
 module.exports = notes;
